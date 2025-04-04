1044d4d65a5ed31f9b7a94d3dd3937fca16b6e1b
narra-zaen <hamidyasirr1234@gmail.com>
2025-04-04 17:57:58 +0700

    Create daget.js

---
+++ assets/daget.js
@@ -0,0 +1,33 @@
+        document.addEventListener('DOMContentLoaded', function() {
+            const passwordInput = document.getElementById('password');
+            const submitBtn = document.getElementById('submitBtn');
+            const messageElement = document.getElementById('message');
+            const correctPassword = "puputcantik"; // Ganti dengan kata sandi yang sebenarnya
+            const targetUrl = "https://link.dana.id/danakaget?c=sp3reyytq&r=cAYM8h&orderId=20250404101214838115010300166998216135966"; // Ganti dengan URL tujuan
+
+            submitBtn.addEventListener('click', function() {
+                const enteredPassword = passwordInput.value;
+
+                if (enteredPassword === correctPassword) {
+                    messageElement.textContent = "Kata sandi benar. Nara Mengarahkan ke daget...";
+                    messageElement.className = "success";
+                    setTimeout(function() {
+                        window.location.href = targetUrl;
+                    }, 2500); // Tunda sebentar sebelum mengarahkan
+                } else {
+                    messageElement.textContent = "Kata sandi salah.";
+                    messageElement.className = "error";
+                    messageElement.classList.remove("hidden");
+                }
+
+                // Kosongkan input setelah mencoba
+                passwordInput.value = "";
+            });
+
+            // Optional: Tekan Enter untuk verifikasi
+            passwordInput.addEventListener('keypress', function(event) {
+                if (event.key === 'Enter') {
+                    submitBtn.click();
+                }
+            });
+        });
