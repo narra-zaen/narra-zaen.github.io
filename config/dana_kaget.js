2206cf85d41f19dd53c410451fb5c88f7f7a5845
narra-zaen <hamidyasirr1234@gmail.com>
2025-04-09 18:54:11 +0700

    Update dana_kaget.json

--- config/dana_kaget.js
+++ config/dana_kaget.js
@@ -1,2 +1,3 @@
-// Ini adalah file konfigurasi untuk link Dana Kaget
-const danaKagetLink = "https://link.dana.id/danakaget?c=sn3e8efuq&r=cAYM8h&orderId=20250409101214808715010300166998216995563";
+{
+  "link": "https://link.dana.id/danakaget?c=sn3e8efuq&r=cAYM8h&orderId=20250409101214808715010300166998216995563"
+}
